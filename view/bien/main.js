26548e27a0e8a8db47d5e0a6232eb7a5487827ff
Aleck_lvl <147378013+Aleck-lvl@users.noreply.github.com>
2024-05-02 19:09:55 +0200

    Delete view directory

--- view/bien/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('selectStatut').addEventListener('change', function(){
-    document.getElementById('idStatut').value = document.getElementById('selectStatut').value
-});
-
-document.getElementById('selectTypeBien').addEventListener('change', function(){
-    document.getElementById('idTypeBien').value = document.getElementById('selectTypeBien').value
-});
--- view/tarification/bdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $serveur = 'localhost';
-  $db = 'projet';
-  $user = 'root';
-  $pass = '';
-  $conn = new PDO("mysql:host=$serveur;dbname=$db;charset=utf8", $user, $pass);
-
-?>
